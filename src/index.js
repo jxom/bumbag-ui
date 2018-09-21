13c66d84654cec305b0f7f8109403e73d4b6a985
jxom <jakemoxey@gmail.com>
2018-09-21 21:54:02 +1000

    Export Reakit primitives

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-export { Button } from './Button';
-export { Buttons } from './Buttons';
-export { Spinner } from './Spinner';
+export { default as Box } from 'reakit/Box';
+export { default as Block } from 'reakit/Block';
+export { default as InlineBlock } from 'reakit/InlineBlock';
+export { default as Flex } from 'reakit/Flex';
+export { default as InlineFlex } from 'reakit/InlineFlex';
+export { default as Grid } from 'reakit/Grid';
 
 export { default as theme } from './theme';
 export { default as ThemeProvider } from './theme/ThemeProvider';
+
+export { Button } from './Button';
+export { Buttons } from './Buttons';
+export { Spinner } from './Spinner';
