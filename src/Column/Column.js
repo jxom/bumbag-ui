d1259ac8640e7c008b030401c7e631c98e363889
jxom <jakemoxey@gmail.com>
2018-09-26 00:21:18 +1000

    Add sizes and offsets to columns

--- src/Column/Column.js
+++ src/Column/Column.js
@@ -3,19 +3,19 @@ import PropTypes from 'prop-types';
 
 import _Column from './styled';
 
-const Column = ({ children, className, ...props }) => (
-  <_Column className={className} {...props}>
-    {children}
-  </_Column>
-);
+const Column = ({ children, ...props }) => <_Column {...props}>{children}</_Column>;
 
 Column.propTypes = {
   children: PropTypes.node.isRequired,
-  className: PropTypes.string
+  className: PropTypes.string,
+  offsetSpread: PropTypes.oneOf([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 'left', 'both', 'right']),
+  spread: PropTypes.oneOf([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
 };
 
 Column.defaultProps = {
-  className: null
+  className: null,
+  offsetSpread: null,
+  spread: null
 };
 
 export default Column;
--- src/Column/styled.js
+++ src/Column/styled.js
@@ -1,12 +1,49 @@
-// import { css } from 'styled-components';
+import { css } from 'styled-components';
 import styled from 'reakit/styled';
 import Box from 'reakit/Box';
 import { theme } from 'styled-tools';
+
+const marginAutoOffsets = {
+  left: css`
+    margin-left: auto;
+  `,
+  both: css`
+    margin-left: auto;
+    margin-right: auto;
+  `,
+  right: css`
+    margin-right: auto;
+  `
+};
+
+const getOffsetSpreadAttributes = offsetSpread => {
+  if (typeof offsetSpread === 'number') {
+    return css`
+      margin-left: ${getWidth(offsetSpread)};
+    `;
+  }
+  return marginAutoOffsets[offsetSpread];
+};
+
+const getWidth = spread => `${spread / 12 * 100}%`;
 
 const Column = styled(Box)`
   flex: 1;
   max-width: 100%;
   padding: ${theme('layout.gapFactor')}rem;
+
+  ${props =>
+    props.spread &&
+    css`
+      & {
+        flex: none;
+        width: ${getWidth(props.spread)};
+      }
+    `};
+
+  & {
+    ${props => props.offsetSpread && getOffsetSpreadAttributes(props.offsetSpread)};
+  }
 `;
 
 export default Column;
