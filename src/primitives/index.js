fc41571f41179613dc7666ab899af4de2fe362e5
jxom <jakemoxey@gmail.com>
2018-10-01 21:09:23 +1000

    Refactor docs imports

---
+++ src/primitives/index.js
@@ -0,0 +1,6 @@
+export { default as Box } from 'reakit/Box';
+export { default as Block } from 'reakit/Block';
+export { default as InlineBlock } from 'reakit/InlineBlock';
+export { default as Flex } from 'reakit/Flex';
+export { default as InlineFlex } from 'reakit/InlineFlex';
+export { default as Grid } from 'reakit/Grid';
