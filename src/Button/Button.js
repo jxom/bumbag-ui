ee986aa31e771fe1759bf073bab1a0cb5d0e618e
Dave Olsen <daivols@gmail.com>
2018-10-29 17:16:50 +1100

    fix height of loading buttons

--- src/Button/Button.js
+++ src/Button/Button.js
@@ -9,8 +9,6 @@ import _Button from './styled';
 
 export const SpinnerWrapper = styled(Box)`
   position: absolute;
-  width: 100%;
-  height: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
