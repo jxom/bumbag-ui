4b1acadafdf4cf18693a1bebbe2f812897f799a3
jxom <jakemoxey@gmail.com>
2019-03-26 16:59:55 +1100

    Update Medipass theme

--- src/themes/medipass/index.ts
+++ src/themes/medipass/index.ts
@@ -4,7 +4,9 @@ import layout from './layout';
 import fontWeights from './fontWeights';
 import _palette from './palette';
 
+import ActionButtons from './ActionButtons';
 import Alert from './Alert';
+import Button from './Button';
 import Checkbox from './Checkbox';
 import Container from './Container';
 import Heading from './Heading';
@@ -20,7 +22,9 @@ export default {
   palette: _palette,
 
   // Components
+  ActionButtons,
   Alert,
+  Button,
   Checkbox,
   Container,
   Heading,
