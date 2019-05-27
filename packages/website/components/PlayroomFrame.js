1b2f89ffb57ca3dcf3ff31b45664caae442c0e63
jxom <jakemoxey@gmail.com>
2019-05-27 16:04:04 +1000

    Fix broken playroom

--- packages/website/components/PlayroomFrame.js
+++ packages/website/components/PlayroomFrame.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import { Box, ThemeProvider, StyleSheetManager } from 'fannypack';
+import { Box, ThemeProvider } from 'fannypack';
 
 export default function Frame({ children, frameWindow }) {
   return (
-    <StyleSheetManager target={frameWindow.document.head}>
-      <ThemeProvider>
-        <Box>{children}</Box>
-      </ThemeProvider>
-    </StyleSheetManager>
+    <ThemeProvider>
+      <Box>{children}</Box>
+    </ThemeProvider>
   );
 }
