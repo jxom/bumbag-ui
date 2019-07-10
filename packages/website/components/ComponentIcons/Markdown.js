18d064cb7086246fc6666e38270fdbb466a2de64
Samantha Wong <sw@medipass.io>
2019-07-10 11:40:10 +1000

    add missing component icons (#86)

---
+++ packages/website/components/ComponentIcons/Markdown.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SvgComponent = props => (
+  <svg width="78" height="70" viewBox="0 0 78 70" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M3.25 21.6H13.65L22.25 38.3L30.65 21.6H41.05V56H30.65V37.65L24.3 49.65H20.05L13.65 37.65V56H3.25V21.6Z"
+      fill="#3926A5"
+    />
+    <line x1="63" y1="21" x2="63" y2="43" stroke="#3926A5" strokeWidth="10" />
+    <path d="M63 56L75.9904 37.25H50.0096L63 56Z" fill="#3926A5" />
+  </svg>
+);
+
+export default SvgComponent;
