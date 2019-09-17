cd3342546be4467c13f076c3c093018046ed336d
Jake Moxey <jakemoxey@gmail.com>
2019-09-18 00:52:07 +1000

    WIP: Standalone Alert

--- packages/fannypack/src/Alert/Alert.tsx
+++ packages/fannypack/src/Alert/Alert.tsx
@@ -16,6 +16,7 @@ export type LocalAlertProps = {
   isInline?: boolean;
   onClickClose?: ButtonProps['onClick'];
   showCloseButton?: boolean;
+  standalone?: boolean;
   title?: string;
   type?: string;
   closeButtonProps?: Omit<ButtonProps, 'children'>;
@@ -23,6 +24,8 @@ export type LocalAlertProps = {
   iconProps?: Omit<IconProps, 'icon'>;
 };
 export type AlertProps = BoxProps & LocalAlertProps;
+
+export const AlertContext = React.createContext<AlertProps & { themeKey?: string }>({});
 
 const useProps = createHook<AlertProps>(
   (props, themeKey) => {
@@ -35,6 +38,7 @@ const useProps = createHook<AlertProps>(
       isInline,
       onClickClose,
       showCloseButton,
+      standalone,
       title,
       type,
       ...restProps
@@ -47,26 +51,6 @@ const useProps = createHook<AlertProps>(
       themeKey,
       prevClassName: boxProps.className
     });
-    const alertContentClassName = useClassName({
-      style: styles.AlertContent,
-      styleProps: props,
-      themeKey: `${themeKey}.Content`
-    });
-    const alertTitleClassName = useClassName({
-      style: styles.AlertTitle,
-      styleProps: props,
-      themeKey: `${themeKey}.Title`
-    });
-    const alertDescriptionClassName = useClassName({
-      style: styles.AlertDescription,
-      styleProps: props,
-      themeKey: `${themeKey}.Description`
-    });
-    const alertIconWrapperClassName = useClassName({
-      style: styles.AlertIconWrapper,
-      styleProps: props,
-      themeKey: `${themeKey}.IconWrapper`
-    });
     const alertCloseButtonClassName = useClassName({
       style: styles.AlertCloseButton,
       styleProps: props,
@@ -77,32 +61,20 @@ const useProps = createHook<AlertProps>(
     const descriptionId = useUniqueId('calloutDescription');
 
     const children = (
-      <React.Fragment>
+      <AlertContext.Provider value={props}>
         <Box display="flex" alignItems="center" justifyContent="space-between">
           <Box display="flex" alignItems="center">
-            {hasIcon && (
-              <Box className={alertIconWrapperClassName}>
-                <Icon
-                  aria-hidden
-                  color={isFilled ? `${type}Inverted` : type}
-                  icon={type}
-                  fontSize={props.children && !isInline ? '400' : '200'}
-                  {...iconProps}
-                />
-              </Box>
+            {standalone ? (
+              props.children
+            ) : (
+              <React.Fragment>
+                {hasIcon && <AlertIcon />}
+                <AlertContent>
+                  {title && <AlertTitle titleId={titleId}>{title}</AlertTitle>}
+                  <AlertDescription id={descriptionId}>{props.children}</AlertDescription>
+                </AlertContent>
+              </React.Fragment>
             )}
-            <Box className={alertContentClassName}>
-              {title && (
-                <Box className={alertTitleClassName}>
-                  <Text fontWeight="semibold" id={titleId}>
-                    {title}
-                  </Text>
-                </Box>
-              )}
-              <Box className={alertDescriptionClassName} id={descriptionId}>
-                {props.children}
-              </Box>
-            </Box>
           </Box>
           {showCloseButton && (
             <Box display="flex">
@@ -118,7 +90,7 @@ const useProps = createHook<AlertProps>(
             </Box>
           )}
         </Box>
-      </React.Fragment>
+      </AlertContext.Provider>
     );
 
     return {
@@ -150,3 +122,103 @@ export const Alert = createComponent<AlertProps>(
     themeKey: 'Alert'
   }
 );
+
+/////////////////////////////////////
+
+export type LocalAlertIconProps = {
+  iconProps?: Omit<IconProps, 'icon'>;
+};
+export type AlertIconProps = BoxProps & LocalAlertIconProps;
+
+export function AlertIcon(props: AlertIconProps) {
+  const { iconProps, ...restProps } = props;
+  const context = React.useContext(AlertContext);
+
+  const alertIconWrapperClassName = useClassName({
+    style: styles.AlertIconWrapper,
+    styleProps: { ...context, ...props },
+    themeKey: `${context.themeKey || 'Alert'}.IconWrapper`
+  });
+
+  return (
+    <Box className={alertIconWrapperClassName} {...restProps}>
+      <Icon
+        aria-hidden
+        color={context.isFilled ? `${context.type}Inverted` : context.type}
+        fontSize={props.children || !context.isInline ? '400' : '200'}
+        icon={context.type}
+        {...iconProps}
+      />
+    </Box>
+  );
+}
+
+/////////////////////////////////////
+
+export type LocalAlertContentProps = {};
+export type AlertContentProps = BoxProps & LocalAlertContentProps;
+
+export function AlertContent(props: AlertContentProps) {
+  const { children, ...restProps } = props;
+  const context = React.useContext(AlertContext);
+
+  const alertContentClassName = useClassName({
+    style: styles.AlertContent,
+    styleProps: { ...context, ...props },
+    themeKey: `${context.themeKey || 'Alert'}.Content`
+  });
+
+  return (
+    <Box className={alertContentClassName} {...restProps}>
+      {children}
+    </Box>
+  );
+}
+
+/////////////////////////////////////
+
+export type LocalAlertTitleProps = {
+  titleId?: string;
+};
+export type AlertTitleProps = BoxProps & LocalAlertTitleProps;
+
+export function AlertTitle(props: AlertTitleProps) {
+  const { children, titleId, ...restProps } = props;
+  const context = React.useContext(AlertContext);
+
+  const alertTitleClassName = useClassName({
+    style: styles.AlertTitle,
+    styleProps: { ...context, ...props },
+    themeKey: `${context.themeKey || 'Alert'}.Title`
+  });
+
+  return (
+    <Box className={alertTitleClassName} {...restProps}>
+      <Text fontWeight="semibold" id={titleId}>
+        {children}
+      </Text>
+    </Box>
+  );
+}
+
+/////////////////////////////////////
+
+export type LocalAlertDescriptionProps = {};
+export type AlertDescriptionProps = BoxProps & LocalAlertDescriptionProps;
+
+export function AlertDescription(props: AlertDescriptionProps) {
+  const { children, ...restProps } = props;
+  const context = React.useContext(AlertContext);
+
+  const alertDescriptionClassName = useClassName({
+    style: styles.AlertDescription,
+    styleProps: { ...context, ...props },
+    themeKey: `${context.themeKey || 'Alert'}.Description`
+  });
+
+  return (
+    <Box className={alertDescriptionClassName} {...restProps}>
+      {children}
+    </Box>
+  );
+}
--- packages/fannypack/src/Alert/index.ts
+++ packages/fannypack/src/Alert/index.ts
@@ -1,4 +1,11 @@
 import * as styles from './styles';
+import { Alert as _Alert, AlertIcon, AlertContent, AlertDescription, AlertTitle } from './Alert';
 
 export * from './Alert';
+export const Alert = Object.assign(_Alert, {
+  Icon: AlertIcon,
+  Content: AlertContent,
+  Description: AlertDescription,
+  Title: AlertTitle
+});
 export { styles as alertStyles };
