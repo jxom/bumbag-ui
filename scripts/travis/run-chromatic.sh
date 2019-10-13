f41260a2b9679c0536d69474b0fba03a31ca7079
Jake Moxey <jakemoxey@gmail.com>
2019-10-13 23:40:23 +1100

    Fix travis

--- scripts/travis/run-chromatic.sh
+++ scripts/travis/run-chromatic.sh
@@ -1,4 +1,4 @@
 if [[ $TRAVIS_EVENT_TYPE != 'pull_request' ||  $TRAVIS_PULL_REQUEST_SLUG != $TRAVIS_REPO_SLUG ]];
 then
-  cd ../packages/storybook && yarn chromatic
+  cd ../../packages/storybook && yarn chromatic
 fi
