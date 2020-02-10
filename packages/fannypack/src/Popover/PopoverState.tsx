e70e9f5cd07f28613da30b65e43091daffd72666
Jake Moxey <jakemoxey@gmail.com>
2020-02-10 16:48:34 +1100

    WIP

--- packages/fannypack/src/Popover/PopoverState.tsx
+++ packages/fannypack/src/Popover/PopoverState.tsx
@@ -1,1 +1,10 @@
-export { usePopoverState, PopoverStateReturn, PopoverInitialState } from 'reakit';
+import {
+  usePopoverState,
+  PopoverStateReturn as ReakitPopoverStateReturn,
+  PopoverInitialState as ReakitPopoverInitialState
+} from 'reakit';
+
+export type PopoverStateReturn = ReakitPopoverStateReturn;
+export type PopoverInitialState = ReakitPopoverInitialState;
+
+export { usePopoverState };
