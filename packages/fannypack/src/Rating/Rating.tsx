a3b721e40f8392dc869e06b2c8d9004d649d0cb7
Jake Moxey <jakemoxey@gmail.com>
2020-02-19 16:18:17 +1100

    WIP

--- packages/fannypack/src/Rating/Rating.tsx
+++ packages/fannypack/src/Rating/Rating.tsx
@@ -1,18 +1,31 @@
 import * as React from 'react';
 import { Box as ReakitBox } from 'reakit';
+import _times from 'lodash/times';
 
 import { useClassName, createComponent, createElement, createHook } from '../utils';
 import { Box, BoxProps } from '../Box';
+import { Rover } from '../Rover';
 import { Icon, IconProps } from '../Icon';
 
 import * as styles from './styles';
 
-export type LocalRatingProps = {};
+export type LocalRatingProps = {
+  color?: string;
+  disabled?: boolean;
+  maxValue?: number;
+  onChange: (index: number) => void;
+  value: number | void;
+};
 export type RatingProps = BoxProps & LocalRatingProps;
 
 const useProps = createHook<RatingProps>(
   (props, { themeKey, themeKeyOverride }) => {
-    const boxProps = Box.useProps(props);
+    const { disabled, onChange, maxValue, value, ...restProps } = props;
+    const boxProps = Box.useProps(restProps);
+
+    const rover = Rover.useState();
+
+    const [hoveringIndex, setHoveringIndex] = React.useState(-1);
 
     const className = useClassName({
       style: styles.Rating,
@@ -22,20 +35,94 @@ const useProps = createHook<RatingProps>(
       prevClassName: boxProps.className
     });
 
-    return { ...boxProps, className, children: <Icon icon="star" /> };
+    return {
+      ...boxProps,
+      className,
+      onMouseLeave: !disabled ? () => setHoveringIndex(-1) : undefined,
+      role: 'radiogroup',
+      children: _times(maxValue, index => (
+        <Rover {...rover} disabled={disabled}>
+          {props => (
+            /*
+            // @ts-ignore */
+            <RatingItem
+              key={index}
+              {...props}
+              aria-setsize={maxValue}
+              aria-posinset={index + 1}
+              aria-checked={value === index + 1}
+              isActive={hoveringIndex >= 0 ? hoveringIndex >= index : value > index}
+              onClick={!disabled ? () => onChange(index + 1) : undefined}
+              onMouseEnter={!disabled ? () => setHoveringIndex(index) : undefined}
+            />
+          )}
+        </Rover>
+      ))
+    };
   },
-  { themeKey: 'Rating' }
+  { defaultProps: { maxValue: 5, value: 0 }, themeKey: 'Rating' }
 );
 
 export const Rating = createComponent<RatingProps>(
   props => {
-    const textProps = useProps(props);
-    return createElement({ children: props.children, component: ReakitBox, use: props.use, htmlProps: textProps });
+    const ratingProps = useProps(props);
+    return createElement({ children: props.children, component: ReakitBox, use: props.use, htmlProps: ratingProps });
   },
   {
     attach: {
       useProps
     },
     themeKey: 'Rating'
+  }
+);
+
+////////////////////////////////////////
+
+export type LocalRatingItemProps = {
+  isActive?: boolean;
+};
+export type RatingItemProps = BoxProps & LocalRatingItemProps;
+
+const useRatingItemProps = createHook<RatingItemProps>(
+  (props, { themeKey, themeKeyOverride }) => {
+    const boxProps = Box.useProps(props);
+
+    const className = useClassName({
+      style: styles.RatingItem,
+      styleProps: props,
+      themeKey,
+      themeKeyOverride,
+      prevClassName: boxProps.className
+    });
+
+    return {
+      ...boxProps,
+      className,
+      role: 'radio',
+      children: (
+        /*
+        // @ts-ignore */
+        <Icon icon="star" />
+      )
+    };
+  },
+  { themeKey: 'Rating.Item' }
+);
+
+export const RatingItem = createComponent<RatingItemProps>(
+  props => {
+    const ratingItemProps = useRatingItemProps(props);
+    return createElement({
+      children: props.children,
+      component: ReakitBox,
+      use: props.use,
+      htmlProps: ratingItemProps
+    });
+  },
+  {
+    attach: {
+      useProps
+    },
+    themeKey: 'Rating.Item'
   }
 );
--- packages/fannypack/src/Rating/styles.ts
+++ packages/fannypack/src/Rating/styles.ts
@@ -1,7 +1,41 @@
-import { cssClass } from '../styled';
-import { theme } from '../utils';
+import { css, cssClass } from '../styled';
+import { palette, fontSize, space, theme } from '../utils';
 
 export const Rating = styleProps => cssClass`
+  align-items: center;
+  display: flex;
+
+  & {
+    ${theme(`${styleProps.themeKey}.css.root`)(styleProps)};
+  }
+`;
+
+export const RatingItem = styleProps => cssClass`
+  color: ${styleProps.isActive ? 'gold' : palette('white900')(styleProps)};
+  display: inline-flex;
+  font-size: ${fontSize('400')(styleProps)}rem;
+  transition: color 0.1s, transform 0.2s;
+
+  ${!styleProps.disabled &&
+    css`
+      &:hover {
+        transform: scale(1.2);
+      }
+      &:hover:active {
+        transform: scale(1.1);
+      }
+    `}
+
+  ${styleProps.disabled &&
+    css`
+      cursor: not-allowed;
+      opacity: 0.5;
+    `}
+
+  &:not(:first-of-type) {
+    margin-left: ${space(1)(styleProps)}rem;
+  }
+
   & {
     ${theme(`${styleProps.themeKey}.css.root`)(styleProps)};
   }
