f4b6382dd8af686d076cb49213af06850151c48c
Jake Moxey <jakemoxey@gmail.com>
2020-05-13 10:14:38 +1000

    WIP

--- packages/fannypack/src/Autosuggest/Autosuggest.tsx
+++ packages/fannypack/src/Autosuggest/Autosuggest.tsx
@@ -1,12 +1,21 @@
 import * as React from 'react';
 import { Box as ReakitBox } from 'reakit';
 import _pick from 'lodash/pick';
-import { useEffectReducer } from 'use-effect-reducer';
+import _get from 'lodash/get';
 
 import { useClassName, createComponent, createElement, createHook } from '../utils';
 import { Box, BoxProps } from '../Box';
+import { Button, ButtonProps } from '../Button';
 import { Input, InputProps } from '../Input';
-import { DropdownMenu, DropdownMenuPopover, DropdownMenuDisclosure, DropdownMenuItem } from '../DropdownMenu';
+import {
+  DropdownMenu,
+  DropdownMenuPopover,
+  DropdownMenuDisclosure,
+  DropdownMenuItem,
+  DropdownMenuPopoverProps,
+  DropdownMenuItemProps,
+  DropdownMenuInitialState
+} from '../DropdownMenu';
 import { Text, TextProps } from '../Text';
 
 import * as styles from './styles';
@@ -14,51 +23,39 @@ import * as styles from './styles';
 export type LocalAutosuggestProps = {
   options: any;
   placeholder?: InputProps['placeholder'];
+
   inputProps?: Partial<InputProps>;
+  itemProps?: Partial<DropdownMenuItemProps>;
+  popoverProps?: Partial<DropdownMenuPopoverProps>;
+
+  dropdownMenuInitialState?: Partial<DropdownMenuInitialState>;
 };
 export type AutosuggestProps = BoxProps & LocalAutosuggestProps;
 
+const KEY_ENTER = 13;
 const KEY_ESC = 27;
 const KEY_UP = 38;
 const KEY_DOWN = 40;
 
-function reducer(state, event, exec) {
+function reducer(state, event) {
   switch (event.type) {
     case 'INPUT_CHANGE': {
-      exec({ type: 'filterOptions', searchText: event.value });
-
       return {
         ...state,
         highlightedIndex: -1,
-        inputValue: event.value
+        inputValue: event.value,
+        selectedOption: undefined
       };
     }
     case 'INPUT_BLUR': {
-      exec({ type: 'hideDropdownMenu' });
-
       return {
-        ...state
-      };
-    }
-    case 'INPUT_CLICK': {
-      exec({ type: 'showDropdownMenu' });
-
-      return {
-        ...state
-      };
-    }
-    case 'INPUT_FOCUS': {
-      exec({ type: 'showDropdownMenu' });
-
-      return {
-        ...state
+        ...state,
+        highlightedIndex: -1
       };
     }
     case 'KEY_UP': {
       const newHighlightedIndex =
         state.highlightedIndex > 0 ? state.highlightedIndex - 1 : state.filteredOptions.length - 1;
-
-      exec({ type: 'showDropdownMenu' });
 
       return {
         ...state,
@@ -70,8 +67,6 @@ function reducer(state, event, exec) {
       const newHighlightedIndex =
         state.highlightedIndex < state.filteredOptions.length - 1 ? state.highlightedIndex + 1 : 0;
 
-      exec({ type: 'showDropdownMenu' });
-
       return {
         ...state,
         highlightedIndex: newHighlightedIndex,
@@ -79,18 +74,36 @@ function reducer(state, event, exec) {
       };
     }
     case 'KEY_ESC': {
-      exec({ type: 'hideDropdownMenu' });
-
       return { ...state, highlightedIndex: -1, inputValue: '' };
     }
+    case 'KEY_ENTER': {
+      return { ...state, highlightedIndex: -1 };
+    }
+    case 'MOUSE_ENTER_ITEM': {
+      const newHighlightedIndex = event.index;
+      return {
+        ...state,
+        highlightedIndex: newHighlightedIndex
+      };
+    }
+    case 'MOUSE_LEAVE_ITEM': {
+      const newHighlightedIndex = state.highlightedIndex === event.index ? -1 : state.highlightedIndex;
+      return {
+        ...state,
+        highlightedIndex: newHighlightedIndex
+      };
+    }
+    case 'MOUSE_CLICK_ITEM': {
+      return { ...state, highlightedIndex: -1, inputValue: state.filteredOptions[event.index].label };
+    }
     case 'OPTIONS_FILTERED': {
-      if (event.filteredOptions.length === 0) {
-        exec({ type: 'hideDropdownMenu' });
-      } else {
-        exec({ type: 'showDropdownMenu' });
-      }
-
       return { ...state, filteredOptions: event.filteredOptions };
+    }
+    case 'OPTION_SELECTED': {
+      return { ...state, filteredOptions: [event.option], selectedOption: event.option };
+    }
+    case 'OPTION_CLEARED': {
+      return { ...state, filteredOptions: state.options, inputValue: '', selectedOption: undefined };
     }
     default: {
       return state;
@@ -100,7 +113,7 @@ function reducer(state, event, exec) {
 
 const useProps = createHook<AutosuggestProps>(
   (props, { themeKey, themeKeyOverride }) => {
-    const { inputProps, options, placeholder, ...restProps } = props;
+    const { dropdownMenuInitialState, popoverProps, itemProps, inputProps, options, placeholder, ...restProps } = props;
 
     //////////////////////////////////////////////////
 
@@ -108,7 +121,7 @@ const useProps = createHook<AutosuggestProps>(
 
     //////////////////////////////////////////////////
 
-    const dropdownMenu = DropdownMenu.useState({ gutter: 4 });
+    const dropdownMenu = DropdownMenu.useState({ loop: true, gutter: 4, ...dropdownMenuInitialState });
     const dropdownMenuDisclosureProps = DropdownMenuDisclosure.useProps(dropdownMenu);
 
     //////////////////////////////////////////////////
@@ -137,85 +150,135 @@ const useProps = createHook<AutosuggestProps>(
 
     //////////////////////////////////////////////////
 
+    const [{ highlightedIndex, inputValue, filteredOptions, selectedOption }, dispatch] = React.useReducer(reducer, {
+      highlightedIndex: -1,
+      inputValue: '',
+      filteredOptions: options,
+      options
+    });
+
+    //////////////////////////////////////////////////
+
     const filterOptions = React.useCallback(
-      (state, effect, dispatch) => {
-        const { searchText } = effect;
+      ({ controlsVisibility, searchText }) => {
         const filteredOptions = options.filter(option =>
           option.label.toLowerCase().includes(searchText.trim().toLowerCase())
         );
-        dispatch({ type: 'OPTIONS_FILTERED', filteredOptions });
-      },
-      [options]
-    );
-
-    const [{ highlightedIndex, inputValue, filteredOptions }, dispatch] = useEffectReducer(
-      reducer,
-      {
-        highlightedIndex: -1,
-        inputValue: '',
-        filteredOptions: options
-      },
-      {
-        showDropdownMenu: () => {
-          if (!dropdownMenu.visible) {
+        if (controlsVisibility) {
+          if (filteredOptions.length === 0) {
+            dropdownMenu.hide();
+          } else {
             dropdownMenu.show();
           }
-        },
-        hideDropdownMenu: () => {
-          dropdownMenu.hide();
-        },
-        filterOptions
-      }
+        }
+        dispatch({ type: 'OPTIONS_FILTERED', filteredOptions, controlsVisibility });
+        return filteredOptions;
+      },
+      [dispatch, dropdownMenu, options]
+    );
+
+    const selectOption = React.useCallback(
+      ({ index }) => {
+        const option = filteredOptions[index];
+        dispatch({ type: 'OPTION_SELECTED', option });
+        return option;
+      },
+      [dispatch, filteredOptions]
     );
 
     //////////////////////////////////////////////////
 
-    const handleBlur = React.useCallback(
+    const handleBlurInput = React.useCallback(
       event => {
-        dispatch({ type: 'INPUT_BLUR' });
+        const value = event.target.value;
+        dispatch({ type: 'INPUT_BLUR', value });
+        if (highlightedIndex === -1) {
+          filterOptions({ searchText: value });
+          dropdownMenu.hide();
+        }
       },
-      [dispatch]
+      [dispatch, dropdownMenu, filterOptions, highlightedIndex]
     );
 
-    const handleChange = React.useCallback(
+    const handleChangeInput = React.useCallback(
       event => {
         const value = event.target.value;
         dispatch({ type: 'INPUT_CHANGE', value });
+        filterOptions({ controlsVisibility: true, searchText: value });
       },
-      [dispatch]
+      [dispatch, filterOptions]
     );
 
-    const handleClick = React.useCallback(
+    const handleClickInput = React.useCallback(
       event => {
-        dispatch({ type: 'INPUT_CLICK' });
+        const value = event.target.value;
+        filterOptions({ controlsVisibility: true, searchText: value });
       },
-      [dispatch]
+      [filterOptions]
     );
 
-    const handleFocus = React.useCallback(
+    const handleFocusInput = React.useCallback(
       event => {
-        dispatch({ type: 'INPUT_FOCUS' });
+        dropdownMenu.show();
       },
-      [dispatch]
+      [dropdownMenu]
     );
 
-    const handleKeyDown = React.useCallback(
+    const handleKeyDownInput = React.useCallback(
       event => {
         if (event.keyCode === KEY_ESC) {
           event.preventDefault();
+          dropdownMenu.hide();
           dispatch({ type: 'KEY_ESC' });
         }
         if (event.keyCode === KEY_DOWN) {
           event.preventDefault();
+          dropdownMenu.show();
           dispatch({ type: 'KEY_DOWN' });
         }
         if (event.keyCode === KEY_UP) {
           event.preventDefault();
+          dropdownMenu.show();
           dispatch({ type: 'KEY_UP' });
         }
+        if (event.keyCode === KEY_ENTER) {
+          event.preventDefault();
+          dispatch({ type: 'KEY_ENTER' });
+          if (highlightedIndex >= 0) {
+            dropdownMenu.hide();
+            selectOption({ index: highlightedIndex });
+          }
+        }
+      },
+      [dispatch, dropdownMenu, highlightedIndex, selectOption]
+    );
+
+    const handleClickItem = React.useCallback(
+      index => () => {
+        dispatch({ type: 'MOUSE_CLICK_ITEM', index });
+        dropdownMenu.hide();
+        selectOption({ index });
+      },
+      [dispatch, dropdownMenu, selectOption]
+    );
+
+    const handleMouseEnterItem = React.useCallback(
+      index => () => {
+        dispatch({ type: 'MOUSE_ENTER_ITEM', index });
       },
       [dispatch]
     );
+
+    const handleMouseLeaveItem = React.useCallback(
+      index => () => {
+        dispatch({ type: 'MOUSE_LEAVE_ITEM', index });
+      },
+      [dispatch]
+    );
+
+    const handleClear = React.useCallback(() => {
+      dispatch({ type: 'OPTION_CLEARED' });
+    }, []);
 
     //////////////////////////////////////////////////
 
@@ -229,13 +292,20 @@ const useProps = createHook<AutosuggestProps>(
         <React.Fragment>
           <Input
             {..._pick(dropdownMenuDisclosureProps, 'aria-controls', 'ref')}
+            after={
+              selectedOption && (
+                <Box display="flex" alignItems="center" justify-content="center" paddingY="minor-1" paddingX="minor-2">
+                  <Button.Close onClick={handleClear} iconProps={{ fontSize: '200' }} size="small" />
+                </Box>
+              )
+            }
             aria-autocomplete="list"
-            // TODO: aria-activedescendant
-            onBlur={handleBlur}
-            onClick={handleClick}
-            onChange={handleChange}
-            onKeyDown={handleKeyDown}
-            onFocus={handleFocus}
+            aria-activedescendant={_get(dropdownMenu, `items[${highlightedIndex}].id`)}
+            onBlur={handleBlurInput}
+            onClick={handleClickInput}
+            onChange={handleChangeInput}
+            onKeyDown={handleKeyDownInput}
+            onFocus={handleFocusInput}
             placeholder={placeholder}
             value={inputValue}
             {...inputProps}
@@ -249,6 +319,7 @@ const useProps = createHook<AutosuggestProps>(
             hideOnEsc={false}
             hideOnClickOutside={false}
             unstable_autoFocusOnHide={false}
+            {...popoverProps}
           >
             {filteredOptions.map((option, index) => (
               <DropdownMenuItem
@@ -258,7 +329,11 @@ const useProps = createHook<AutosuggestProps>(
                 aria-selected={highlightedIndex === index}
                 className={dropdownMenuItemClassname}
                 isTabbable={false}
+                onClick={handleClickItem(index)}
+                onMouseEnter={handleMouseEnterItem(index)}
+                onMouseLeave={handleMouseLeaveItem(index)}
                 role="option"
+                {...itemProps}
               >
                 <Option label={option.label} inputValue={inputValue} />
               </DropdownMenuItem>
@@ -284,9 +359,23 @@ export const Autosuggest = createComponent<AutosuggestProps>(
   }
 );
 
-function Option(props) {
+function Option(props: { label: string; inputValue: string }) {
   const { label, inputValue } = props;
-  const match = label.toLowerCase().match(inputValue.toLowerCase()) || [];
-  // TODO: Highlight matched text
-  return <Text>{label}</Text>;
+
+  const escapeStringRegexp = string => string.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&');
+  const match = label.match(new RegExp(escapeStringRegexp(inputValue), 'i')) || [];
+
+  const preText = label.slice(0, match.index);
+  const highlightedText = match[0];
+  const postText = label.slice(match.index + (match[0] || '').length);
+
+  return highlightedText ? (
+    <React.Fragment>
+      {preText && <Text>{preText}</Text>}
+      {highlightedText && <Text fontWeight="semibold">{highlightedText}</Text>}
+      {postText && <Text>{postText}</Text>}
+    </React.Fragment>
+  ) : (
+    <Text>{label}</Text>
+  );
 }
