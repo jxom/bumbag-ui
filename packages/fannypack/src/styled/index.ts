86f1dbac0c5c81ddb3e29d577085ff87cd82b44d
Jake Moxey <jakemoxey@gmail.com>
2020-06-25 11:12:40 +1000

    Remove emotion-server as a dep

--- packages/fannypack/src/styled/index.ts
+++ packages/fannypack/src/styled/index.ts
@@ -2,5 +2,4 @@ export { default as classNames } from 'classnames';
 export { flush, hydrate, cx, merge, getRegisteredStyles, injectGlobal, sheet, cache, css as cssClass } from 'emotion';
 export { css, keyframes, Global, ThemeContext, CacheProvider } from '@emotion/core';
 export { withTheme, ThemeProvider } from 'emotion-theming';
-export { extractCritical, renderStylesToString, renderStylesToNodeStream } from 'emotion-server';
 export { default as styled } from '@emotion/styled';
