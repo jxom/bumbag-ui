4cc3867b22c7cb133183715d0fc9f051c4b8e800
Jake Moxey <jakemoxey@gmail.com>
2020-07-28 17:19:19 +1000

    A wild gatsby plugin appeared

--- packages/website/gatsby-ssr.js
+++ packages/website/gatsby-ssr.js
@@ -6,21 +6,4 @@
 
 // You can delete this file if you're not using it
 
-import React from 'react';
-import { renderToString } from 'react-dom/server';
-import { renderStylesToString } from 'emotion-server';
-import { InitializeColorMode } from 'bumbag';
-import ElementWrapper from './src/layout/ElementWrapper';
-
-export const replaceRenderer = ({ bodyComponent, replaceBodyHTMLString }) => {
-  const html = renderStylesToString(renderToString(bodyComponent));
-  replaceBodyHTMLString(html);
-};
-
-export const wrapRootElement = (props) => {
-  return <ElementWrapper {...props} />;
-};
-
-export const onRenderBody = ({ setPreBodyComponents }) => {
-  setPreBodyComponents([<InitializeColorMode key="bumbag-no-flash" />])
-}
+export { wrapRootElement } from './gatsby-browser';
