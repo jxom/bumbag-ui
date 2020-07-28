998dde22c821baee081cc4a2b493bd82dfd99261
Jake Moxey <jakemoxey@gmail.com>
2020-07-28 15:22:02 +1000

    Fix init color mode

--- packages/bumbag/src/ColorMode/InitializeColorMode.tsx
+++ packages/bumbag/src/ColorMode/InitializeColorMode.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 // Thanks to Brent Jackson for this little trick. What a legend.
 const noflash = `(function() { try {
-  var mode = localStorage.getItem('bb-mode');
+  var mode = localStorage.getItem('bb.mode');
   if (!mode) return
   document.body.classList.add('bb-mode-' + mode);
 } catch (e) {} })();`;
