e3662d97343b6796999a6fc12551697455f1b52f
Jake Moxey <jakemoxey@gmail.com>
2020-08-12 10:37:21 +1000

    WIP

--- packages/bumbag/src/theme/lineHeights.ts
+++ packages/bumbag/src/theme/lineHeights.ts
@@ -1,7 +1,7 @@
 import { LineHeightsThemeConfig } from '../types';
 
 export default (overrides: LineHeightsThemeConfig) => ({
+  small: 1.125,
   default: 1.5,
-  heading: 1.125,
   ...overrides,
 });
