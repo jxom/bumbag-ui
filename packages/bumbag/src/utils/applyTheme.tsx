a833da49e262ad93d109628f54ff1a65891d4919
Jake Moxey <jakemoxey@gmail.com>
2020-08-17 10:16:07 +1000

    Make applyTheme compatible with compound components

--- packages/bumbag/src/utils/applyTheme.tsx
+++ packages/bumbag/src/utils/applyTheme.tsx
@@ -2,9 +2,12 @@ import * as React from 'react';
 import _set from 'lodash/set';
 
 export function applyTheme<ThemeConfig>(Comp: React.FunctionComponent<any>, theme: ThemeConfig) {
-  return function <Props>(props: Props) {
+  function Component<Props>(props: Props) {
     // @ts-ignore
     const newOverrides = _set(props.overrides || {}, Comp.displayName, theme);
     return <Comp {...props} overrides={newOverrides} />;
-  };
+  }
+  return Object.assign(Component, {
+    ...Comp,
+  });
 }
