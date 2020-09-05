f39c6389fad03411ccbfa238ab94d7836520febd
Agusti Fernandez <6601142+agustif@users.noreply.github.com>
2020-09-05 16:13:22 +0200

    Update document on next example (#63)
    Expected Document Component Html was not rendered. Make sure you render them in your custom `_document`

--- examples/next-basic/pages/_document.js
+++ examples/next-basic/pages/_document.js
@@ -1,32 +1,34 @@
-import Document, { Head, Main, NextScript } from 'next/document';
-import { extractCritical } from 'bumbag-server';
-import { InitializeColorMode } from 'bumbag';
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { extractCritical } from 'bumbag-server'
+import { InitializeColorMode } from 'bumbag'
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    const styles = extractCritical(initialProps.html);
+    const initialProps = await Document.getInitialProps(ctx)
+    const styles = extractCritical(initialProps.html)
     return {
       ...initialProps,
       styles: (
         <>
           {initialProps.styles}
-          <style dangerouslySetInnerHTML={{ __html: styles.css }} data-emotion-css={styles.ids.join(' ')} />
+          <style
+            data-emotion-css={styles.ids.join(' ')}
+            dangerouslySetInnerHTML={{ __html: styles.css }}
+          />
         </>
       ),
-    };
+    }
   }
-
   render() {
     return (
-      <html>
+      <Html>
         <Head />
         <body>
           <InitializeColorMode />
           <Main />
           <NextScript />
         </body>
-      </html>
+      </Html>
     );
   }
 }
