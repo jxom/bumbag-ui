2609f041b27fdacb1ed0c595ca001bd643c2e058
Jake Moxey <jakemoxey@gmail.com>
2020-09-07 18:05:21 +1000

    Fix open graph tags

--- packages/website/src/components/SEO.tsx
+++ packages/website/src/components/SEO.tsx
@@ -7,24 +7,24 @@ const SEO = (props: any) => {
   const { pathname } = useLocation();
   const seo = {
     title: props.title || 'Bumbag',
-    titleTemplate:
-      pathname === '/' ? `%s | Rapidly build accessible & themeable React applications with ease` : '%s | Bumbag',
+    titleTemplate: pathname === '/' ? `%s` : '%s | Bumbag',
+    description: 'Rapidly build accessible & themeable React applications with ease',
+    image: '/social.png',
+    url: 'https://bumbag.style',
   };
   return (
     <Helmet title={seo.title} titleTemplate={seo.titleTemplate}>
       <link rel="icon" type="image/png" href="/icon.png" />
-      {/* <meta name="description" content={seo.description} /> */}
-      {/* <meta name="image" content={seo.image} /> */}
-      {/* {seo.url && <meta property="og:url" content={seo.url} />} */}
-      {/* {(article ? true : null) && <meta property="og:type" content="article" />} */}
-      {/* {seo.title && <meta property="og:title" content={seo.title} />} */}
-      {/* {seo.description && <meta property="og:description" content={seo.description} />} */}
-      {/* {seo.image && <meta property="og:image" content={seo.image} />} */}
-      {/* <meta name="twitter:card" content="summary_large_image" /> */}
-      {/* {twitterUsername && <meta name="twitter:creator" content={twitterUsername} />} */}
-      {/* {seo.title && <meta name="twitter:title" content={seo.title} />} */}
-      {/* {seo.description && <meta name="twitter:description" content={seo.description} />} */}
-      {/* {seo.image && <meta name="twitter:image" content={seo.image} />} */}
+      <meta name="description" content={seo.description} />
+      <meta name="image" content={seo.image} />
+      <meta property="og:url" content={seo.url} />
+      <meta property="og:title" content={seo.title} />
+      <meta property="og:description" content={seo.description} />
+      <meta property="og:image" content={seo.image} />
+      <meta name="twitter:creator" content="@jxom" />
+      <meta name="twitter:title" content={seo.title} />
+      <meta name="twitter:description" content={seo.description} />
+      <meta name="twitter:image" content={seo.image} />
     </Helmet>
   );
 };
