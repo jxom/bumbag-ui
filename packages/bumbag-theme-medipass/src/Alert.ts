6546161548c33f67628741ff9ed3124e62475f7d
Jake Moxey <jakemoxey@gmail.com>
2020-10-20 14:10:21 +1100

    Add custom padding to Medipass Alert

--- packages/bumbag-theme-medipass/src/Alert.ts
+++ packages/bumbag-theme-medipass/src/Alert.ts
@@ -1,4 +1,13 @@
+import { css, space } from 'bumbag';
+
 export default {
+  Wrapper: {
+    styles: {
+      base: css`
+        padding: ${space(2, 'major')}rem;
+      `,
+    },
+  },
   defaultProps: {
     accent: true,
     variant: 'bordered',
