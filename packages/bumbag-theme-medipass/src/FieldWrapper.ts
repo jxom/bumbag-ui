5ea7144681232f5dc5a94d8c9653343c494f76ce
Jake Moxey <jakemoxey@gmail.com>
2020-11-27 13:22:36 +1100

    Update Input and FieldWrapper theme for Medipass

--- packages/bumbag-theme-medipass/src/FieldWrapper.ts
+++ packages/bumbag-theme-medipass/src/FieldWrapper.ts
@@ -1,4 +1,4 @@
-import { css } from 'bumbag';
+import { css, palette } from 'bumbag';
 
 export default {
   TooltipPopover: {
@@ -11,13 +11,27 @@ export default {
   },
   TooltipTrigger: {
     styles: {
-      base: css`
+      base: (props) => css`
+        border: none;
+        background: unset;
         min-height: unset;
         height: unset;
         padding: 0.1em 0.2em;
         position: absolute;
         top: -0.75em;
         height: 22px;
+
+        &:hover {
+          background: unset;
+          color: ${palette('text')(props)};
+        }
+
+        &&&:focus {
+          background: unset;
+          outline: none;
+          border: none;
+          color: ${palette('text')(props)};
+        }
       `,
     },
   },
--- packages/bumbag-theme-medipass/src/Input.ts
+++ packages/bumbag-theme-medipass/src/Input.ts
@@ -1,11 +1,24 @@
-import { css, space } from 'bumbag';
+import { css, palette, space } from 'bumbag';
 
 export default {
   styles: {
     base: (styleProps) => css`
       border-radius: 0px;
+      border: 1px solid ${palette('gray100')(styleProps)};
       box-shadow: none;
       height: ${space(11)(styleProps)}em;
+
+      ${styleProps.state &&
+      css`
+        & {
+          border-color: ${palette(styleProps.state)(styleProps)};
+          box-shadow: ${palette(styleProps.state)(styleProps)} 0px 0px 0px 1px !important;
+        }
+      `};
+    `,
+    focus: (styleProps) => css`
+      border-color: ${palette('primary')(styleProps)};
+      box-shadow: ${palette('primary')(styleProps)} 0px 0px 0px 1px !important;
     `,
   },
   Icon: {
