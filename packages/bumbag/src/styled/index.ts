7c307af4786d0a2c353cb20f2561d54664b03039
Jake Moxey <jake@coinjar.com>
2021-01-07 23:11:26 +1100

    WIP

--- packages/bumbag/src/styled/index.ts
+++ packages/bumbag/src/styled/index.ts
@@ -1,5 +1,21 @@
+import createEmotion from 'create-emotion';
+
 export { default as classNames } from 'classnames';
-export { flush, hydrate, cx, getRegisteredStyles, injectGlobal, sheet, cache, css as cssClass } from 'emotion';
 export { css, keyframes, Global, ThemeContext, CacheProvider } from '@emotion/core';
 export { withTheme, ThemeProvider } from 'emotion-theming';
 export { default as styled } from '@emotion/styled';
+
+const noop = () => {};
+const { flush, hydrate, cx, getRegisteredStyles, injectGlobal, sheet, cache, css } = window.document
+  ? createEmotion()
+  : {
+      flush: noop,
+      hydrate: noop,
+      cx: noop,
+      getRegisteredStyles: noop,
+      injectGlobal: noop,
+      sheet: {},
+      cache: {},
+      css: noop,
+    };
+export { flush, hydrate, cx, getRegisteredStyles, injectGlobal, sheet, cache, css as cssClass };
