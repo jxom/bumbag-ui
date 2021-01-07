9770119f328f86929aefd918bf5409ffc3dfe0f9
Jake Moxey <jake@coinjar.com>
2021-01-07 23:13:25 +1100

    yadadada

--- packages/bumbag/src/styled/index.ts
+++ packages/bumbag/src/styled/index.ts
@@ -6,16 +6,17 @@ export { withTheme, ThemeProvider } from 'emotion-theming';
 export { default as styled } from '@emotion/styled';
 
 const noop = () => {};
-const { flush, hydrate, cx, getRegisteredStyles, injectGlobal, sheet, cache, css } = window.document
-  ? createEmotion()
-  : {
-      flush: noop,
-      hydrate: noop,
-      cx: noop,
-      getRegisteredStyles: noop,
-      injectGlobal: noop,
-      sheet: {},
-      cache: {},
-      css: noop,
-    };
+const { flush, hydrate, cx, getRegisteredStyles, injectGlobal, sheet, cache, css } =
+  window && window.document
+    ? createEmotion()
+    : {
+        flush: noop,
+        hydrate: noop,
+        cx: noop,
+        getRegisteredStyles: noop,
+        injectGlobal: noop,
+        sheet: {},
+        cache: {},
+        css: noop,
+      };
 export { flush, hydrate, cx, getRegisteredStyles, injectGlobal, sheet, cache, css as cssClass };
