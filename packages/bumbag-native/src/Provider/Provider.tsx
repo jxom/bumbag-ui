38d5569156b8123f93fb3afd97826622a1637963
Jake Moxey <jake@coinjar.com>
2021-01-07 23:46:59 +1100

    WIP

--- packages/bumbag-native/src/Provider/Provider.tsx
+++ packages/bumbag-native/src/Provider/Provider.tsx
@@ -1,12 +1,57 @@
-import React from 'react';
-import { Platform } from 'react-native';
-import { Provider as BumbagProvider } from 'bumbag/Provider';
+import * as React from 'react';
 
-type Props = {
-  children: any;
+import { ThemeProvider as EmotionProvider } from 'bumbag/styled';
+import buildTheme from 'bumbag/theme';
+import { LayoutBreakpoint, ThemeConfig } from 'bumbag/types';
+
+import { ColorModeProvider } from 'bumbag/ColorMode/ColorModeContext';
+import { BumbagThemeContext } from 'bumbag/Provider/ThemeContext';
+
+export type ProviderProps = {
+  children: React.ReactNode;
+  isStandalone?: boolean;
+  collapseBelow?: LayoutBreakpoint;
+  colorMode?: string;
+  isSSR?: boolean;
+  theme?: ThemeConfig;
 };
 
-export function Provider(props: Props) {
-  const { children } = props;
-  return <BumbagProvider platform={Platform.OS === 'web' ? 'web' : 'native'}>{children}</BumbagProvider>;
+Provider.defaultProps = {
+  colorMode: 'default',
+};
+
+export function Provider(props: ProviderProps) {
+  const { children, colorMode, isSSR, isStandalone, theme: _theme } = props;
+
+  ////////////////////////////////////////////////
+
+  const [theme, setTheme] = React.useState(_theme);
+  React.useEffect(() => {
+    setTheme(_theme);
+  }, [_theme]);
+
+  ////////////////////////////////////////////////
+
+  const derivedTheme = React.useMemo(() => {
+    if (theme && isStandalone) {
+      return theme;
+    }
+    return buildTheme(theme);
+  }, [isStandalone, theme]);
+
+  ////////////////////////////////////////////////
+
+  const themeContextValue = React.useMemo(() => ({ theme, setTheme }), [theme, setTheme]);
+
+  ////////////////////////////////////////////////
+
+  return (
+    <BumbagThemeContext.Provider value={themeContextValue}>
+      <EmotionProvider theme={derivedTheme}>
+        <ColorModeProvider isSSR={isSSR} mode={colorMode}>
+          {children}
+        </ColorModeProvider>
+      </EmotionProvider>
+    </BumbagThemeContext.Provider>
+  );
 }
