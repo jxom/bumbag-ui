fb5e2a3f56a9e27ada0fd3d4fc87c8dfff64fadd
Jake Moxey <jakemoxey@gmail.com>
2021-02-25 22:39:27 +1100

    WIP

--- packages/bumbag-native/src/Input/Input.tsx
+++ packages/bumbag-native/src/Input/Input.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
-import { ColorValue, Platform, TextInputProps as RNTextInputProps } from 'react-native';
-import { Input as WebInput } from 'bumbag/Input';
+import { TextInputProps as RNTextInputProps } from 'react-native';
 import {
   createComponent,
   createElement,
@@ -42,142 +41,130 @@ const useProps = createHook<InputProps>(
   { defaultProps, themeKey: 'native.Input' }
 );
 
-export const Input =
-  false && Platform.OS === 'web'
-    ? WebInput
-    : createComponent<InputProps>(
-        (props) => {
-          const { label, labelProps, size } = props;
-          const fontSize = props.fontSize || styles.SIZES[size];
+export const Input = createComponent<InputProps>(
+  (props) => {
+    const { label, labelProps, size } = props;
+    const fontSize = props.fontSize || styles.SIZES[size];
 
-          /////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////
 
-          const inputRef = React.useRef({ value: null });
-          const [isFocused, setIsFocused] = React.useState(false);
+    const inputRef = React.useRef({ value: null });
+    const [isFocused, setIsFocused] = React.useState(false);
 
-          /////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////
 
-          const handleBlur = React.useCallback(
-            (e) => {
-              setIsFocused(false);
-              props.onBlur && props.onBlur(e);
-            },
-            [props]
-          );
+    const handleBlur = React.useCallback(
+      (e) => {
+        setIsFocused(false);
+        props.onBlur && props.onBlur(e);
+      },
+      [props]
+    );
 
-          const handlePress = React.useCallback(() => {
-            setIsFocused(true);
-            // @ts-ignore
-            inputRef.current && inputRef.current.focus();
-          }, []);
+    const handlePress = React.useCallback(() => {
+      setIsFocused(true);
+      // @ts-ignore
+      inputRef.current && inputRef.current.focus();
+    }, []);
 
-          /////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////
 
-          const htmlProps = useProps(
-            omitCSSProps({
-              ...props,
-              ref: inputRef,
-              onBlur: handleBlur,
-              placeholder: !label || props.disabled ? props.placeholder || props.label : '',
-            })
-          );
-          const element = createElement({
-            children: props.children,
-            component: styles.StyledInput,
-            htmlProps: { ...htmlProps, fontSize },
-          });
+    const htmlProps = useProps(
+      omitCSSProps({
+        ...props,
+        ref: inputRef,
+        onBlur: handleBlur,
+        placeholder: !label || props.disabled ? props.placeholder || props.label : '',
+      })
+    );
+    const element = createElement({
+      children: props.children,
+      component: styles.StyledInput,
+      htmlProps: { ...htmlProps, fontSize },
+    });
 
-          /////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////
 
-          const labelStartColor = usePalette(props.placeholderTextColor || props.palette || 'gray300');
-          const labelEndColor = usePalette(props.labelTextColor || props.palette || 'gray300');
-          const topStart = useSpace(0.8, fontSize);
-          const topEnd = useSpace(-0.5, fontSize);
-          const fontSizeStart = useSpace(1, fontSize);
-          const fontSizeEnd = useSpace(0.75, fontSize);
+    const labelStartColor = usePalette(props.placeholderTextColor || props.palette || 'gray300');
+    const labelEndColor = usePalette(props.labelTextColor || props.palette || 'gray300');
+    const topStart = useSpace(0.8, fontSize);
+    const topEnd = useSpace(-0.5, fontSize);
+    const fontSizeStart = useSpace(1, fontSize);
+    const fontSizeEnd = useSpace(0.75, fontSize);
 
-          const colorAnimation = React.useRef(new Animated.Value(0)).current;
-          const topAnimation = React.useRef(new Animated.Value(topStart)).current;
-          const fontSizeAnimation = React.useRef(new Animated.Value(fontSizeStart)).current;
+    const colorAnimation = React.useRef(new Animated.Value(0)).current;
+    const topAnimation = React.useRef(new Animated.Value(topStart)).current;
+    const fontSizeAnimation = React.useRef(new Animated.Value(fontSizeStart)).current;
 
-          React.useEffect(() => {
-            if (isFocused || (inputRef.current && inputRef.current.value)) {
-              Animated.timing(colorAnimation, {
-                toValue: 1,
-                duration: 100,
-                useNativeDriver: true,
-              }).start();
-              Animated.timing(topAnimation, {
-                toValue: topEnd,
-                duration: 100,
-                useNativeDriver: true,
-              }).start();
-              Animated.timing(fontSizeAnimation, {
-                toValue: fontSizeEnd,
-                duration: 100,
-                useNativeDriver: true,
-              }).start();
-            } else {
-              Animated.timing(colorAnimation, {
-                toValue: 0,
-                duration: 100,
-                useNativeDriver: true,
-              }).start();
-              Animated.timing(topAnimation, {
-                toValue: topStart,
-                duration: 100,
-                useNativeDriver: true,
-              }).start();
-              Animated.timing(fontSizeAnimation, {
-                toValue: fontSizeStart,
-                duration: 100,
-                useNativeDriver: true,
-              }).start();
-            }
-          }, [
-            colorAnimation,
-            fontSizeAnimation,
-            fontSizeEnd,
-            fontSizeStart,
-            isFocused,
-            topAnimation,
-            topEnd,
-            topStart,
-          ]);
+    React.useEffect(() => {
+      if (isFocused || (inputRef.current && inputRef.current.value)) {
+        Animated.timing(colorAnimation, {
+          toValue: 1,
+          duration: 100,
+          useNativeDriver: true,
+        }).start();
+        Animated.timing(topAnimation, {
+          toValue: topEnd,
+          duration: 100,
+          useNativeDriver: true,
+        }).start();
+        Animated.timing(fontSizeAnimation, {
+          toValue: fontSizeEnd,
+          duration: 100,
+          useNativeDriver: true,
+        }).start();
+      } else {
+        Animated.timing(colorAnimation, {
+          toValue: 0,
+          duration: 100,
+          useNativeDriver: true,
+        }).start();
+        Animated.timing(topAnimation, {
+          toValue: topStart,
+          duration: 100,
+          useNativeDriver: true,
+        }).start();
+        Animated.timing(fontSizeAnimation, {
+          toValue: fontSizeStart,
+          duration: 100,
+          useNativeDriver: true,
+        }).start();
+      }
+    }, [colorAnimation, fontSizeAnimation, fontSizeEnd, fontSizeStart, isFocused, topAnimation, topEnd, topStart]);
 
-          /////////////////////////////////////////////////////////////////////
+    const color = colorAnimation.interpolate({
+      inputRange: [0, 1],
+      outputRange: [labelStartColor, labelEndColor],
+    });
 
-          const color = colorAnimation.interpolate({
-            inputRange: [0, 1],
-            outputRange: [labelStartColor, labelEndColor],
-          });
+    /////////////////////////////////////////////////////////////////////
 
-          return (
-            <Pressable onPress={handlePress} position="relative" {...pickCSSProps(props)}>
-              <>
-                {label && !props.disabled && (
-                  <styles.StyledAnimatedLabel
-                    color={color}
-                    top={topAnimation}
-                    fontSize={fontSizeAnimation}
-                    variant={props.variant}
-                    _web={{ cursor: 'text' }}
-                    {...labelProps}
-                  >
-                    {label}
-                  </styles.StyledAnimatedLabel>
-                )}
-                {element}
-              </>
-            </Pressable>
-          );
-        },
-        {
-          attach: {
-            useProps,
-            displayName: 'native.Input',
-          },
-          defaultProps,
-          themeKey: 'native.Input',
-        }
-      );
+    return (
+      <Pressable onPress={handlePress} position="relative" {...pickCSSProps(props)}>
+        <>
+          {label && !props.disabled && (
+            <styles.StyledAnimatedLabel
+              color={color}
+              top={topAnimation}
+              fontSize={fontSizeAnimation}
+              variant={props.variant}
+              _web={{ cursor: 'text' }}
+              {...labelProps}
+            >
+              {label}
+            </styles.StyledAnimatedLabel>
+          )}
+          {element}
+        </>
+      </Pressable>
+    );
+  },
+  {
+    attach: {
+      useProps,
+      displayName: 'native.Input',
+    },
+    defaultProps,
+    themeKey: 'native.Input',
+  }
+);
