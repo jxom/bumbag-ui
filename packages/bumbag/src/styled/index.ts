e37c7fb96cbdf5ca92b28b88f0875cf89882cb76
Chris Chuck <chrischuck@Chriss-MacBook-Pro.local>
2021-03-12 14:36:42 -0800

    Readd emotion

--- packages/bumbag/src/styled/index.ts
+++ packages/bumbag/src/styled/index.ts
@@ -1,5 +1,6 @@
 export { default as classNames } from "classnames";
-export { flush, hydrate, cx, getRegisteredStyles, injectGlobal, sheet, cache, css as cssClass } from "@emotion/css";
+export { cache } from 'emotion';
+export { flush, hydrate, cx, getRegisteredStyles, injectGlobal, sheet, css as cssClass } from "@emotion/css";
 export { css, keyframes, Global, ThemeContext, CacheProvider } from "@emotion/react";
 export { withTheme, ThemeProvider } from "@emotion/react";
 export { default as styled } from "@emotion/styled";
