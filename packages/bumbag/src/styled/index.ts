9a0aadeece766a2ed707baf2112146a049c3dc87
Chris Chuck <chrischuck@Chriss-MacBook-Pro.local>
2021-03-12 14:54:49 -0800

    Fix typos and formatting

--- packages/bumbag/src/styled/index.ts
+++ packages/bumbag/src/styled/index.ts
@@ -1,6 +1,6 @@
-export { default as classNames } from "classnames";
+export { default as classNames } from 'classnames';
 export { cache } from 'emotion';
-export { flush, hydrate, cx, getRegisteredStyles, injectGlobal, sheet, css as cssClass } from "@emotion/css";
-export { css, keyframes, Global, ThemeContext, CacheProvider } from "@emotion/react";
-export { withTheme, ThemeProvider } from "@emotion/react";
-export { default as styled } from "@emotion/styled";
+export { flush, hydrate, cx, getRegisteredStyles, injectGlobal, sheet, css as cssClass } from '@emotion/css';
+export { css, keyframes, Global, ThemeContext, CacheProvider } from '@emotion/react';
+export { withTheme, ThemeProvider } from '@emotion/react';
+export { default as styled } from '@emotion/styled';
