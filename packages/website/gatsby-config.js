a345f4278a0865e5deabda64104c5e2e53e4aab4
Chris Chuck <chrischuck@Chriss-MacBook-Pro.local>
2021-03-12 14:21:33 -0800

    Re-add emotion

--- packages/website/gatsby-config.js
+++ packages/website/gatsby-config.js
@@ -1,68 +1,68 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = {
   developMiddleware: (app) => {
     app.use(
-      '/playroom/',
+      "/playroom/",
       createProxyMiddleware({
-        target: 'http://localhost:9000',
+        target: "http://localhost:9000",
         pathRewrite: {
-          '/playroom/': '',
+          "/playroom/": "",
         },
       })
     );
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-typescript',
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-typescript",
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: 'pages',
+        name: "pages",
         path: `${__dirname}/src/pages`,
       },
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: 'docs',
+        name: "docs",
         path: `${__dirname}/src/docs`,
       },
     },
+    // {
+    //   resolve: 'gatsby-plugin-manifest',
+    //   options: {
+    //     name: 'Bumbag',
+    //     short_name: 'Bumbag',
+    //     start_url: '/',
+    //     background_color: '#663399',
+    //     theme_color: '#663399',
+    //     display: 'minimal-ui',
+    //     // icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+    //   },
+    // },
     {
-      resolve: 'gatsby-plugin-manifest',
-      options: {
-        name: 'Bumbag',
-        short_name: 'Bumbag',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        // icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
-      },
-    },
-    {
-      resolve: 'gatsby-plugin-mdx',
+      resolve: "gatsby-plugin-mdx",
       options: {
         gatsbyRemarkPlugins: [
           {
-            resolve: 'gatsby-remark-autolink-headers',
+            resolve: "gatsby-remark-autolink-headers",
             options: {
               offsetY: 76,
-              icon: '<span>#</span>',
-              elements: ['h1', 'h2', 'h3', 'h4'],
+              icon: "<span>#</span>",
+              elements: ["h1", "h2", "h3", "h4"],
             },
           },
         ],
       },
     },
-    'gatsby-plugin-offline',
-    'gatsby-plugin-bumbag',
-    'gatsby-plugin-webpack-bundle-analyzer',
+    "gatsby-plugin-offline",
+    "gatsby-plugin-bumbag",
+    "gatsby-plugin-webpack-bundle-analyzer",
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: 'UA-172675851-1',
+        trackingId: "UA-172675851-1",
         defer: true,
       },
     },
