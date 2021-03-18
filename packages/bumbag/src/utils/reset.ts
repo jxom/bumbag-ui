e6d3a011569d6e8f40483a96ace0e1692d4f5835
Jake Moxey <jakemoxey@gmail.com>
2021-03-18 15:33:13 +1100

    Fix

--- packages/bumbag/src/utils/reset.ts
+++ packages/bumbag/src/utils/reset.ts
@@ -1,19 +1,6 @@
 if (typeof window !== 'undefined') {
-  const css = `.bb-Box {
-    margin: unset;
-    padding: unset;
-    border: unset;
-    background: unset;
-    font: unset;
-    font-family: inherit;
-    font-size: 100%;
-    box-sizing: border-box;
-  }
-  .bb-Box:focus:not(:focus-visible) {
-    outline: none;
-  }
-  `;
+  const css = `.bb-Box{margin: unset;padding: unset;border: unset;background: unset;font: unset;font-family: inherit;font-size: 100%;box-sizing: border-box;}.bb-Box:focus:not(:focus-visible) {outline: none;}`;
   let style = document.createElement('style');
   style.appendChild(document.createTextNode(css));
-  document.head.insertBefore(style, document.head.firstChild);
+  document.head.insertBefore(style, document.getElementsByTagName('style')[0]);
 }
