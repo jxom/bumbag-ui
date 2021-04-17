785e3a0ebe0fd086639af73a161d51d587bd552d
Joshua Crowley <joshua.crowley@gmail.com>
2021-04-17 11:26:39 +1000

    Fix for darkmode docs palette colour (#116)
    * Tweaks to show off dark theme in Palette docs
    
    So I've updated the Palette page to reflect the colour mode of the docs site. I've added a button so it's easy to toggle, and a bit more info about colour modes.
    
    I don't know if the way I'm accessing the dark colours makes sense? But it works!
    
    * Update palette.mdx
    
    * Fixes for prettier lint issues

--- packages/website/src/components/ColorModePicker.tsx
+++ packages/website/src/components/ColorModePicker.tsx
@@ -1,28 +1,37 @@
 import * as React from 'react';
-import { useLocation, useNavigate } from '@reach/router';
 import { Box, Button, Icon, useColorMode } from 'bumbag';
 
 export default function ColorModePicker(props) {
   const { colorMode, setColorMode } = useColorMode();
 
-  const handleChangeTheme = React.useCallback(
-    (e) => {
-      const colorMode = e.target.value;
-      setColorMode(colorMode);
-    },
-    [setColorMode]
-  );
-
   return (
     <Box>
       {colorMode === 'default' && (
-        <Button onClick={() => setColorMode('dark')} color="gray" variant="ghost">
-          <Icon icon="solid-moon" />
+        <Button
+          onClick={() => setColorMode('dark')}
+          color="gray"
+          iconAfter={props.labeled ? 'solid-moon' : null}
+          variant={props.labeled ? null : 'ghost'}
+        >
+          {props.labeled ? (
+            'Change to Dark Color Mode'
+          ) : (
+            <Icon icon="solid-moon" />
+          )}
         </Button>
       )}
       {colorMode === 'dark' && (
-        <Button onClick={() => setColorMode('default')} color="text" variant="ghost">
-          <Icon icon="solid-sun" />
+        <Button
+          onClick={() => setColorMode('default')}
+          color='text'
+          iconAfter={props.labeled ? 'solid-sun' : null}
+          variant={props.labeled ? null : 'ghost'}
+        >
+          {props.labeled ? (
+            'Change to Default Color Mode'
+          ) : (
+            <Icon icon="solid-sun" />
+          )}
         </Button>
       )}
     </Box>
--- packages/website/src/components/PaletteColor.tsx
+++ packages/website/src/components/PaletteColor.tsx
@@ -1,20 +1,31 @@
 import React from 'react';
-import { Box, Button, Flex, Text, useTheme } from 'bumbag';
+import { Box, Button, Flex, Text, useTheme, useColorMode } from 'bumbag';
 
 const PaletteColor = ({ palette, ...props }: any) => {
   const { theme } = useTheme();
+  const { colorMode } = useColorMode();
+
+  const colorModePalette =
+    colorMode === 'dark' && theme.palette.modes.dark[palette]
+      ? theme.palette.modes.dark[palette]
+      : theme.palette[palette];
 
   return (
     <Box border="default" borderColor="white800">
       <Flex flexDirection="column">
-        <Box height="60px" backgroundColor={palette} width="100%" {...props} />
+        <Box
+          height="60px"
+          backgroundColor={colorModePalette}
+          width="100%"
+          {...props}
+        />
         <Box padding="minor-2" lineHeight="none">
           <Text fontSize="150" marginBottom="minor-1">
             {palette}
           </Text>
           <br />
           <Text color="text100" fontSize="100">
-            {theme.palette[palette]}
+            {colorModePalette}
           </Text>
         </Box>
       </Flex>
