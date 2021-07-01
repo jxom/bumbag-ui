68a5ca818691c76abf3175ed5d97d092727ffcb6
Claudia <claudianadalin98@gmail.com>
2021-07-01 15:31:54 +1000

    fix: google fonts on safari

--- packages/bumbag/src/styled/index.ts
+++ packages/bumbag/src/styled/index.ts
@@ -1,4 +1,5 @@
 export { default as classNames } from 'classnames';
+export { Global } from '@emotion/core';
 export { flush, hydrate, cx, getRegisteredStyles, injectGlobal, sheet, css as cssClass, cache } from '@emotion/css';
-export { css, keyframes, Global, ThemeContext, CacheProvider, withTheme, ThemeProvider } from '@emotion/react';
+export { css, keyframes, ThemeContext, CacheProvider, withTheme, ThemeProvider } from '@emotion/react';
 export { default as styled } from '@emotion/styled';
