06a01b6a8b50fc6ec540e53021571e2ac1096685
Jake Moxey <jakemoxey@gmail.com>
2021-08-17 11:30:54 +1000

    Revert gatsby upgrade

--- packages/website/gatsby-browser.js
+++ packages/website/gatsby-browser.js
@@ -13,6 +13,6 @@ export const wrapRootElement = (props) => {
   return <ElementWrapper {...props} />;
 };
 
-export const wrapPageElement = ({ props }) => {
+export const replaceComponentRenderer = ({ props }) => {
   return React.createElement(props.pageResources.component, props);
 };
