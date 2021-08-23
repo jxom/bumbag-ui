05363b4da58097da052c5986a1148f3c1d77e859
Jake Moxey <jakemoxey@gmail.com>
2021-08-23 10:12:15 +1000

    Fix playroom

--- website/playroom.config.js
+++ website/playroom.config.js
@@ -1,42 +1,51 @@
 const path = require('path');
 
 module.exports = {
-  // TODO: target for bumbag-native
-  components: '../packages/bumbag/src/index.ts',
-  // components: '../bumbag-native/src/index.ts',
+  ...(process.env.NODE_ENV === 'production'
+    ? {
+        components: './node_modules/bumbag/es/index.js',
+      }
+    : {}),
+  ...(process.env.NODE_ENV === 'development'
+    ? {
+        // TODO: target for bumbag-native
+        components: '../packages/bumbag/src/index.ts',
+        // components: '../bumbag-native/src/index.ts',
+        webpackConfig: () => ({
+          module: {
+            rules: [
+              {
+                test: /\.(js|ts|tsx)$/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    cwd: path.join(__dirname, '..'),
+                    configFile: path.resolve(__dirname, './node_modules/playroom/.babelrc'),
+                  },
+                },
+              },
+            ],
+          },
+          resolve: {
+            alias: {
+              bumbag: path.resolve(__dirname, '../packages/bumbag/src'),
+              'bumbag-native': path.resolve(__dirname, '../packages/bumbag-native/src'),
+              'bumbag-addon-markdown': path.resolve(__dirname, '../packages/bumbag-addon-markdown/src'),
+              'bumbag-addon-highlighted-code': path.resolve(__dirname, '../packages/bumbag-addon-highlighted-code/src'),
+              'bumbag-theme-medipass': path.resolve(__dirname, '../packages/bumbag-theme-medipass/src'),
+              'react-native$': 'react-native-web',
+              codemirror: path.resolve(__dirname, './node_modules/codemirror'),
+              reakit: path.resolve(__dirname, './node_modules/reakit'),
+            },
+            extensions: ['.web.js', '.mjs', '.js', '.json', '.ts', '.tsx'],
+            modules: [path.resolve(__dirname, '../node_modules'), 'node_modules'],
+          },
+        }),
+      }
+    : {}),
   outputPath: './out/playroom',
   frameComponent: './components/PlayroomFrame.js',
   widths: [375, 768, 1024, 1200, 1440],
-  webpackConfig: () => ({
-    module: {
-      rules: [
-        {
-          test: /\.(js|ts|tsx)$/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              cwd: path.join(__dirname, '..'),
-              configFile: path.resolve(__dirname, './node_modules/playroom/.babelrc'),
-            },
-          },
-        },
-      ],
-    },
-    resolve: {
-      alias: {
-        bumbag: path.resolve(__dirname, '../packages/bumbag/src'),
-        'bumbag-native': path.resolve(__dirname, '../packages/bumbag-native/src'),
-        'bumbag-addon-markdown': path.resolve(__dirname, '../packages/bumbag-addon-markdown/src'),
-        'bumbag-addon-highlighted-code': path.resolve(__dirname, '../packages/bumbag-addon-highlighted-code/src'),
-        'bumbag-theme-medipass': path.resolve(__dirname, '../packages/bumbag-theme-medipass/src'),
-        'react-native$': 'react-native-web',
-        codemirror: path.resolve(__dirname, './node_modules/codemirror'),
-        reakit: path.resolve(__dirname, './node_modules/reakit'),
-      },
-      extensions: ['.web.js', '.mjs', '.js', '.json', '.ts', '.tsx'],
-      modules: [path.resolve(__dirname, '../node_modules'), 'node_modules'],
-    },
-  }),
   exampleCode: `
     <PageContent>
       <Heading fontSize="500">
